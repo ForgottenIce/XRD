fe8e6e78998ecf622f8d06517f2a077b60b05aa0
bobbobogaming <42958005+bobbobogaming@users.noreply.github.com>
2024-10-22 14:00:50 +0200

    created : scripts and setup for spot pointer

---
+++ AR/AR-Project/Assets/Scripts/packingSpacePointerController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using TMPro;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+
+public class packingSpacePointerController : MonoBehaviour
+{
+
+    [SerializeField] GameObject XROrigin;
+    [SerializeField] GameObject pointer;
+    private GameObject parckingArea;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        var obj = GameObject.Find("ParkingSpaceInput");
+        if (obj != null) {
+            obj.GetComponent<TMP_InputField>().onValueChanged.AddListener(SearchForParkingSpace);
+        }
+    }
+
+    private void SearchForParkingSpace(string arg0) {
+        StartCoroutine(GetCylinder());
+    }
+
+    private IEnumerator GetCylinder() {
+        yield return new WaitForEndOfFrame();
+        parckingArea = XROrigin.GetNamedChild("Cylinder");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (parckingArea != null && parckingArea.activeSelf) {
+            pointer.SetActive(true);
+            pointer.transform.LookAt(parckingArea.transform.position);
+        } else {
+            pointer.SetActive(false);
+        }
+    }
+}
